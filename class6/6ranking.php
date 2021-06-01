8347da2cb125411f656d7d1694fb9056d9f1015d
Natsuki NOGUCHI <61526498+Momo227@users.noreply.github.com>
2021-06-01 10:50:15 +0900

    fix 6.ranking

--- class6/6ranking.php
+++ class6/6ranking.php
@@ -1,4 +1,5 @@
 <?php
+if( isset($_GET['_a']) ){
  $file = file("6.txt");
  foreach($file as $f){
  list($date, $name, $score) = explode(',', rtrim($f));
@@ -8,4 +9,31 @@
  foreach($db as $key => $val) {
  echo "$key,$val\n";
  }
-?> 
+}else if( isset($_GET['_d']) ){
+ $file = file("6.txt");
+ foreach($file as $f){
+ list($date, $name, $score) = explode(',', rtrim($f));
+ list($day, $time) = explode(' ', $date);
+  if( $day == date('y/m/d') ){
+ $db[$date.','.$name] = $score;
+ }
+ asort($db); //タイムが短い順にソートする
+}
+ foreach($db as $key => $val) {
+ echo "$key,$val\n";
+ }
+}else if( isset($_GET['_m']) ){
+ $file = file("6.txt");
+ foreach($file as $f){
+ list($date, $name, $score) = explode(',', rtrim($f));
+ list($y, $m, $time) = explode('/', $date);
+  if( $y.'/'.$m == date('y/m') ){
+ $db[$date.','.$name] = $score;
+ }
+ asort($db); //タイムが短い順にソートする
+}
+ foreach($db as $key => $val) {
+ echo "$key,$val\n";
+ }
+}
+?>
